cd38937c4450256755f85b46f714beda88fd4cf4
qzhongmun <qzhong@mun.ca>
2023-04-01 14:55:04 -0230

    add map

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,85 +1,9 @@
-import React, { Component, useState, useMemo, useCallback } from "react";
+import React, { Component, useState } from "react";
 import Fade from "react-reveal";
 import { Icon } from "leaflet";
-import {
-  MapContainer,
-  TileLayer,
-  Marker,
-  Popup,
-  Rectangle,
-  useMap,
-  useMapEvent,
-} from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 let id = 0;
-const POSITION_CLASSES = {
-  bottomleft: "leaflet-bottom leaflet-left",
-  bottomright: "leaflet-bottom leaflet-right",
-  topleft: "leaflet-top leaflet-left",
-  topright: "leaflet-top leaflet-right",
-};
-
-const BOUNDS_STYLE = { weight: 1 };
-
-function MinimapBounds({ parentMap, zoom }) {
-  const minimap = useMap();
-
-  // Clicking a point on the minimap sets the parent's map center
-  const onClick = useCallback(
-    (e) => {
-      parentMap.setView(e.latlng, parentMap.getZoom());
-    },
-    [parentMap]
-  );
-  useMapEvent("click", onClick);
-
-  // Keep track of bounds in state to trigger renders
-  const [bounds, setBounds] = useState(parentMap.getBounds());
-  const onChange = useCallback(() => {
-    setBounds(parentMap.getBounds());
-    // Update the minimap's view to match the parent map's center and zoom
-    minimap.setView(parentMap.getCenter(), zoom);
-  }, [minimap, parentMap, zoom]);
-
-  // Listen to events on the parent map
-  const handlers = useMemo(() => ({ move: onChange, zoom: onChange }), []);
-  useEventHandlers({ instance: parentMap }, handlers);
-
-  return <Rectangle bounds={bounds} pathOptions={BOUNDS_STYLE} />;
-}
-
-function MinimapControl({ position, zoom }) {
-  const parentMap = useMap();
-  const mapZoom = zoom || 0;
-
-  // Memoize the minimap so it's not affected by position changes
-  const minimap = useMemo(
-    () => (
-      <MapContainer
-        style={{ height: 80, width: 80 }}
-        center={parentMap.getCenter()}
-        zoom={mapZoom}
-        dragging={false}
-        doubleClickZoom={false}
-        scrollWheelZoom={false}
-        attributionControl={false}
-        zoomControl={false}
-      >
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        <MinimapBounds parentMap={parentMap} zoom={mapZoom} />
-      </MapContainer>
-    ),
-    []
-  );
-
-  const positionClass =
-    (position && POSITION_CLASSES[position]) || POSITION_CLASSES.topright;
-  return (
-    <div className={positionClass}>
-      <div className="leaflet-control leaflet-bar">{minimap}</div>
-    </div>
-  );
-}
 
 class Portfolio extends Component {
   constructor(props) {
@@ -113,7 +37,7 @@ class Portfolio extends Component {
                   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                   attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 />
-                <MinimapControl position="topright" />
+
                 {/* <Marker key={0} position={[-20, 30]} icon={icon} /> */}
                 {position.map((para) => (
                   <Marker position={para}>
